8d9490c2da72679d82d41b17bf85f0d9f947829e
sv742744 <shashi09op@gmail.com>
2024-12-06 12:33:13 +0530

    Add files via upload

---
+++ squerpaatern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class squerpaatern{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print((char)(j+64)+" ");//for printting numbers use only j//
+            
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ trainglepattern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class trainglepattern {
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=i;j<=5;j++){
+                System.out.print((char)(j+64)+" ");//for printting numbers use only j//
+            
+            }
+            System.out.println();
+        }
+    }
+}
